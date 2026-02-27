4cabadc3b833cd64f8714665914804e5a319700a
Sameer Abbas <sameerabb121472@gmail.com>
2026-02-27 15:15:43 +0500

    funtions

---
+++ funtion.js
@@ -0,0 +1,46 @@
+// console.log("hello world");
+// function grade(Marks) {
+//   if (Marks > 100) {
+//     console.log("undifined");
+//   } else if (Marks < 1) {
+//     console.log("undifined");
+//   } else if (Marks >= 90) {
+//     console.log("result A+");
+//   } else if (Marks >= 80) {
+//     console.log("result A");
+//   } else if (Marks >= 60) {
+//     console.log("result B");
+//   } else if (Marks >= 40) {
+//     console.log("result C");
+//   } else {
+//     console.log("fail");
+//   }
+// }
+// grade(40);
+// grade(10);
+// grade(70);
+// grade(60);
+
+// standeredfuntioin
+function add(a, b) {
+  return a + b;
+}
+
+let z = add(2, 5);
+console.log(z);
+
+// funtionExprestion
+
+let sub = function (a, b) {
+  return a - b;
+};
+sub(3, 6);
+console.log(sub(22, 7));
+
+// arrow-funtion/
+let b = (c, b) => {
+  return c / b;
+};
+console.log(b(3, 6));
+
+let a = (a, b) => a * b;
